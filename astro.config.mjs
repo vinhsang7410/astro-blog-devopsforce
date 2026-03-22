42898f683703141f79509b3564358398cfeba640
vinhsang7410 <36439454+vinhsang7410@users.noreply.github.com>
2026-03-22 14:19:33 +0700

    add search box

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import pagefind from "astro-pagefind";
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://example.com",
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), pagefind()],
 	adapter: cloudflare({
 		platformProxy: {
 			enabled: true,
